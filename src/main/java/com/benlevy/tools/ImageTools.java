ed17c660b65c04a0ea7a690f43600601455fa2a2
Ben Levy <contact@benlevy.com>
2015-09-03 06:50:32 -0500

    Added some simple functions for getting GPS coordinates

--- src/main/java/com/benlevy/tools/ImageTools.java
+++ src/main/java/com/benlevy/tools/ImageTools.java
@@ -2,8 +2,10 @@ package com.benlevy.tools;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
+import com.drew.lang.Rational;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.exif.GpsDirectory;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,6 +27,42 @@ public class ImageTools {
 			return cal;
 		} catch (NullPointerException ex) {
 			return null;
+		}
+	}
+
+	public static double getJpegLongitude(String filePath) throws IOException, ImageProcessingException {
+		File jpegFile = new File(filePath);
+		Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
+		try {
+			GpsDirectory dir = metadata.getDirectory(GpsDirectory.class);			
+			Rational[] longitudeArray = dir.getRationalArray(GpsDirectory.TAG_GPS_LONGITUDE);
+			double longitude = longitudeArray[0].doubleValue() + longitudeArray[1].doubleValue()/60 + longitudeArray[2].doubleValue()/3600;
+			if (dir.getString(GpsDirectory.TAG_GPS_LONGITUDE_REF).equals("W")) {
+				return - longitude;
+			}
+			else {
+				return longitude;
+			}
+		} catch (NullPointerException ex) {
+			throw new NullPointerException("No GPS data was found for this image");
+		}
+	}
+
+	public static double getJpegLatitude(String filePath) throws IOException, ImageProcessingException {
+		File jpegFile = new File(filePath);
+		Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
+		try {
+			GpsDirectory dir = metadata.getDirectory(GpsDirectory.class);
+			Rational[] latitudeArray = dir.getRationalArray(GpsDirectory.TAG_GPS_LATITUDE);
+			double latitude = latitudeArray[0].doubleValue() + latitudeArray[1].doubleValue()/60 + latitudeArray[2].doubleValue()/3600;
+			if (dir.getString(GpsDirectory.TAG_GPS_LATITUDE_REF).equals("S")) {
+				return - latitude;
+			}
+			else {
+				return latitude;
+			}
+		} catch (NullPointerException ex) {
+			throw new NullPointerException("No GPS data was found for this image");
 		}
 	}
 }
--- src/test/java/com/benlevy/tools/ImageToolsTest.java
+++ src/test/java/com/benlevy/tools/ImageToolsTest.java
@@ -26,4 +26,26 @@ public class ImageToolsTest {
     	assertEquals("month pic taken should be January", 0, cal.get(Calendar.MONTH));
     	assertEquals("day pic taken should be 2nd", 2, cal.get(Calendar.DAY_OF_MONTH));
     }
+    
+    @Test
+    public void testGpsCoordinates() throws ImageProcessingException, IOException {
+    	double longitude = ImageTools.getJpegLongitude(IMAGE_DIRECTORY + "img_3970.jpg");
+    	assertEquals("the longitude should be -76.8872", -76.8872, longitude, 0.0001);
+    	double latitude = ImageTools.getJpegLatitude(IMAGE_DIRECTORY + "img_3970.jpg");
+    	assertEquals("the longitude should be 17.9074", 17.9074, latitude, 0.0001);
+    	try {
+    		longitude = ImageTools.getJpegLongitude(IMAGE_DIRECTORY + "img_9815.jpg");
+    		assertTrue("an image without exif info for longitude should throw a NullPointerException", false);
+    	}
+    	catch (Exception e){
+    		assertTrue(e.getClass().equals(NullPointerException.class));
+    	}
+    	try {
+    		latitude = ImageTools.getJpegLatitude(IMAGE_DIRECTORY + "img_9815.jpg");
+    		assertTrue("an image without exif info for longitude should throw a NullPointerException", false);
+    	}
+    	catch (Exception e){
+    		assertTrue(e.getClass().equals(NullPointerException.class));
+    	}
+    }
 }
